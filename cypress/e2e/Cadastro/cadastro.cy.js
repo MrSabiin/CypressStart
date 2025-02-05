beb2f922ddb4dc1870edd5763aaeef6e1655644c
MrSabiin <marcosbrito.arlequim@gmail.com>
2025-02-04 23:24:03 -0300

    adicionando comandos como check(), select no código para selecionar opções

--- cypress/e2e/Cadastro/cadastro.cy.js
+++ cypress/e2e/Cadastro/cadastro.cy.js
@@ -3,7 +3,7 @@
 context('Cadastro', () => {
   it('Cadastro de usuário', () => {
     cy.visit('https://demo.automationtesting.in/Register.html')
-    
+
     cy.get('input[placeholder="First Name"]')
       .type('Aluno');
 
@@ -19,6 +19,43 @@ context('Cadastro', () => {
     cy.get('input[ng-model^=Phone]')
       .type('1234567890');
 
+    // Radio and Checks
+    cy.get('input[value^=Male]')
+      .click()
 
+    cy.get('input[type=checkbox')
+      .check('Hockey')
+
+    cy.get('input[id="checkbox1"]')
+      .check()
+
+    cy.get('input[type=checkbox]')
+    .check('Movies')
+
+    // Dropdowns & Combos
+    cy.get('select[id="Skills"]')
+    .select('Javascript')
+
+    // Verificar pq não está pegando o campo pelo elemento
+    cy.get('#country')
+    .select('Australia')
+    
+    cy.get('select[id^=yearbox]')
+    .select('1992')
+
+    cy.get('select[ng-model^=monthbox]')
+    .select('February')
+
+    cy.get('select[id^=daybox]')
+    .select('25')
+
+    cy.get('input[id*=password]')
+    .select('Abc123')
+
+    cy.get('input[id^=second]')
+    .select('Abc123')
+
+    //cy.get('');
+    
   });
 });
